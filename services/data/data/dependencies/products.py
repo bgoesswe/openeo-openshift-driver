3793891ce194a18824fcd18c641bb12800beb489
Gunnar Busch <gunnar.busch@eodc.eu>
2018-09-14 14:28:59 +0200

    Removed aliases

--- services/data/data/dependencies/products.py
+++ services/data/data/dependencies/products.py
@@ -3,14 +3,6 @@
 products = [
     {
         'product_id': 's2a_prd_msil1c', 
-        'aliases': [
-            "s2a_prd_msil1c",
-            "sentinel2",
-            "sentinel 2",
-            "sentinel-2", 
-            "sentinel-2a",
-            "sentinel-2b"
-        ],
         'description': ('Sentinel-2 is a multispectral, high-resolution, optical imaging mission, '
             'developed by the European Space Agency (ESA) in the frame of the Copernicus program of the '
             'European Commission.'), 
@@ -18,9 +10,6 @@ products = [
     }, 
     {
         'product_id': 's1a_csar_grdh_iw', 
-        'aliases': [
-            "s1a_csar_grdh_iw"
-        ],
         'description': ('Sentinel-1 is an imaging radar mission providing continuous all-weather, '
             'day-and-night imagery at C-band. The Sentinel-1 constellation provides high reliability, '
             'improved revisit time, geographical coverage and rapid data dissemination to support '
@@ -30,9 +19,6 @@ products = [
     }, 
     {
         'product_id': 's1a_csar_grdm_ew', 
-        'aliases': [
-            "s1a_csar_grdm_ew"
-        ],
         'description': ('Sentinel-1 is an imaging radar mission providing continuous all-weather, '
             'day-and-night imagery at C-band. The Sentinel-1 constellation provides high reliability, '
             'improved revisit time, geographical coverage and rapid data dissemination to support '
@@ -41,9 +27,6 @@ products = [
         'source': 'ESA'}, 
     {
         'product_id': 's1a_csar_slc__ew', 
-        'aliases': [
-            "s1a_csar_slc__ew"
-        ],
         'description': ('Sentinel-1 is an imaging radar mission providing continuous all-weather, '
             'day-and-night imagery at C-band. The Sentinel-1 constellation provides high reliability, '
             'improved revisit time, geographical coverage and rapid data dissemination to support '
@@ -53,9 +36,6 @@ products = [
     },
     {
         'product_id': 's1a_csar_slc__iw',
-        'aliases': [
-            "s1a_csar_slc__iw"
-        ],
         'description': ('Sentinel-1 is an imaging radar mission providing continuous all-weather, '
             'day-and-night imagery at C-band. The Sentinel-1 constellation provides high reliability, '
             'improved revisit time, geographical coverage and rapid data dissemination to support '
@@ -64,10 +44,7 @@ products = [
         'source': 'ESA'
     }, 
     {
-        'product_id': 's1b_csar_grdh_ew', 
-        'aliases': [
-            "s1b_csar_grdh_ew"
-        ],
+        'product_id': 's1b_csar_grdh_ew',
         'description': ('Sentinel-1 is an imaging radar mission providing continuous all-weather, '
             'day-and-night imagery at C-band. The Sentinel-1 constellation provides high reliability, '
             'improved revisit time, geographical coverage and rapid data dissemination to support '
@@ -77,9 +54,6 @@ products = [
     },
     {
         'product_id': 's1b_csar_grdh_iw', 
-        'aliases': [
-            "s1b_csar_grdh_iw"
-        ],
         'description': ('Sentinel-1 is an imaging radar mission providing continuous all-weather, '
             'day-and-night imagery at C-band. The Sentinel-1 constellation provides high reliability, '
             'improved revisit time, geographical coverage and rapid data dissemination to support '
@@ -89,9 +63,6 @@ products = [
     },
     {
         'product_id': 's3a_sl_1_rbt', 
-        'aliases': [
-            "s3a_sl_1_rbt"
-        ],
         'description': ('Sentinel-3 is an European Earth Observation satellite mission developed to '
             'support GMES ocean, land, atmospheric, emergency, security and cryospheric applications.'), 
         'source': 'ESA'
